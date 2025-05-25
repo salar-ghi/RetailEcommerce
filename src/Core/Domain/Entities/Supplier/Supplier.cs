aff6ce9caaa03513b8f3db4252b03fb7fc28535f
salar-ghi <salar.ghi1993@gmail.com>
2025-05-25 18:31:38 +0330

    add supplier status

--- src/Core/Domain/Entities/Supplier/Supplier.cs
+++ src/Core/Domain/Entities/Supplier/Supplier.cs
@@ -8,7 +8,7 @@ public class Supplier : BaseModel<int>
     public string ContactEmail { get; set; }
     public string ContactPhone { get; set; }
     public string Address { get; set; }
-
+    public SupplierStatus status { get; set; } = SupplierStatus.Pending;
     public string UserId { get; set; }
     public User User { get; set; }
     public ICollection<ProductSupplier> ProductSuppliers { get; set; } = new List<ProductSupplier>();
--- src/Core/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ src/Core/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e09c30df283fc3464a7075b8913603321ebf1fa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c313dd21a83ce28a6bd4240786bf1d784ec775e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
