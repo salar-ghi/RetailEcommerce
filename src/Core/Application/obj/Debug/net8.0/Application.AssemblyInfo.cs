6fb8a939f2a237646502a5103497618a39d4ae19
salar-ghi <salar.ghi1993@gmail.com>
2025-05-30 17:59:43 +0330

    minor change

--- src/Core/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ src/Core/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a112bd087bfb97e83e64eee66a94950dd9034c1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab1b471116694f02b9f1a56c1ae7e6a4a41ead4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Core/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ src/Core/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a112bd087bfb97e83e64eee66a94950dd9034c1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab1b471116694f02b9f1a56c1ae7e6a4a41ead4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
