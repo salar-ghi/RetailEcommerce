ca2a2c754fe8e91761812042903d93e981ec7aa1
salar-ghi <salar.ghi1993@gmail.com>
2025-06-02 07:00:01 +0330

    db migration

--- src/Core/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ src/Core/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29634cfed199c1c594505dce473d346dbf6a7d17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea354096146a6ab33bf0a65c08a9f48410e5d54a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
