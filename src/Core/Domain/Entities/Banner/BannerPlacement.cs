d6bedf57aa478497b855c3f77c5ad2d88cef33cc
salar <salar.ghi1993@gmail.com>
2026-02-17 22:49:00 +0330

    minor change

--- src/Core/Domain/Entities/Banner/BannerPlacement.cs
+++ src/Core/Domain/Entities/Banner/BannerPlacement.cs
@@ -3,7 +3,7 @@
 public class BannerPlacement : BaseModel<int>
 {
     public string Name { get; set; } = default!;
-    public string Code { get; set; } = default!;
+    public BannerPageCode Code { get; set; } = default!;
     public string? RecommendedSize { get; set; }
 
     public ICollection<Banner> Banners { get; set; } = new List<Banner>();
---
+++ src/Core/Domain/Enums/BannerPageCode.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Enums;
+
+public enum BannerPageCode
+{
+    HOME_TOP = 1,
+    HOME_BOTTOM = 2,
+    PRODUCT_TOP = 4,
+    PRODUCT_MID = 3,
+    PRODUCT_BOTTOM = 5,
+}
