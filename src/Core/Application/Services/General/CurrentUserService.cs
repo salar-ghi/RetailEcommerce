2fa6956cf90c408f8e1364830d869161f1706099
Salar <salar.ghi1993@gmail.com>
2026-06-05 19:43:06 +0330

    Fix current user claim lookup

--- src/Core/Application/Services/General/CurrentUserService.cs
+++ src/Core/Application/Services/General/CurrentUserService.cs
@@ -3,18 +3,45 @@
 public class CurrentUserService : ICurrentUserService
 {
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private static readonly string[] UserIdClaimTypes =
+    {
+        ClaimTypes.NameIdentifier,
+        JwtRegisteredClaimNames.Sub,
+        "sub",
+        "userId",
+        "uid"
+    };
 
     public CurrentUserService(IHttpContextAccessor httpContextAccessor)
     {
         _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
     }
-    public string UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier)
-        ?? _httpContextAccessor.HttpContext?.User?.FindFirstValue(JwtRegisteredClaimNames.Sub)
-        ?? _httpContextAccessor.HttpContext?.User?.FindFirstValue("sub")
-        ?? _httpContextAccessor.HttpContext?.User?.FindFirstValue("userId")
-        ?? _httpContextAccessor.HttpContext?.User?.FindFirstValue("uid");
+
+    public string UserId => GetUserId() ?? string.Empty;
 
     public IEnumerable<Claim> Claims => _httpContextAccessor.HttpContext?.User?.Claims ?? Enumerable.Empty<Claim>();
+
+    private string? GetUserId()
+    {
+        var user = _httpContextAccessor.HttpContext?.User;
+
+        if (user is null)
+        {
+            return null;
+        }
+
+        foreach (var claimType in UserIdClaimTypes)
+        {
+            var value = user.FindFirst(claimType)?.Value;
+
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                return value;
+            }
+        }
+
+        return null;
+    }
 
     public string GenerateRandomPassword()
     {
