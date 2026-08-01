6b323a22b4c3dd9d22ffefd64a8e3fc06b24d55b
salar <salar.ghi1993@gmail.com>
2026-08-02 00:21:59 +0330

    minor changes

---
+++ src/External/Infrastructure/Migrations/20260801204822_attribuss.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class attribuss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AttributeDefinitions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    DataType = table.Column<int>(type: "int", nullable: false),
+                    Unit = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    IsFilterable = table.Column<bool>(type: "bit", nullable: false),
+                    IsSearchable = table.Column<bool>(type: "bit", nullable: false),
+                    IsComparable = table.Column<bool>(type: "bit", nullable: false),
+                    IsRequired = table.Column<bool>(type: "bit", nullable: false),
+                    IsVariantAttribute = table.Column<bool>(type: "bit", nullable: false),
+                    SortOrder = table.Column<int>(type: "int", nullable: false),
+                    ValidationRegex = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    MinValue = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    MaxValue = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AttributeDefinitions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AttributeOptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AttributeDefinitionId = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Label = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    SortOrder = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AttributeOptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AttributeOptions_AttributeDefinitions_AttributeDefinitionId",
+                        column: x => x.AttributeDefinitionId,
+                        principalTable: "AttributeDefinitions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoryAttributeDefinitions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    AttributeDefinitionId = table.Column<int>(type: "int", nullable: false),
+                    IsRequired = table.Column<bool>(type: "bit", nullable: false),
+                    SortOrder = table.Column<int>(type: "int", nullable: false),
+                    IsVisibleOnProductPage = table.Column<bool>(type: "bit", nullable: false),
+                    IsFilterable = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryAttributeDefinitions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CategoryAttributeDefinitions_AttributeDefinitions_AttributeDefinitionId",
+                        column: x => x.AttributeDefinitionId,
+                        principalTable: "AttributeDefinitions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryAttributeDefinitions_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductAttributeValues",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<long>(type: "bigint", nullable: false),
+                    AttributeDefinitionId = table.Column<int>(type: "int", nullable: false),
+                    StringValue = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: true),
+                    IntValue = table.Column<int>(type: "int", nullable: true),
+                    DecimalValue = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    BoolValue = table.Column<bool>(type: "bit", nullable: true),
+                    DateValue = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    AttributeOptionId = table.Column<int>(type: "int", nullable: true),
+                    AttributeOptionIds = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    ProductId1 = table.Column<long>(type: "bigint", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductAttributeValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductAttributeValues_AttributeDefinitions_AttributeDefinitionId",
+                        column: x => x.AttributeDefinitionId,
+                        principalTable: "AttributeDefinitions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductAttributeValues_AttributeOptions_AttributeOptionId",
+                        column: x => x.AttributeOptionId,
+                        principalTable: "AttributeOptions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ProductAttributeValues_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductAttributeValues_Products_ProductId1",
+                        column: x => x.ProductId1,
+                        principalTable: "Products",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AttributeDefinitions_Code",
+                table: "AttributeDefinitions",
+                column: "Code",
+                unique: true,
+                filter: "[IsDeleted] = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AttributeOptions_AttributeDefinitionId",
+                table: "AttributeOptions",
+                column: "AttributeDefinitionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryAttributeDefinitions_AttributeDefinitionId",
+                table: "CategoryAttributeDefinitions",
+                column: "AttributeDefinitionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryAttributeDefinitions_CategoryId_AttributeDefinitionId",
+                table: "CategoryAttributeDefinitions",
+                columns: new[] { "CategoryId", "AttributeDefinitionId" },
+                unique: true,
+                filter: "[IsDeleted] = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductAttributeValues_AttributeDefinitionId",
+                table: "ProductAttributeValues",
+                column: "AttributeDefinitionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductAttributeValues_AttributeOptionId",
+                table: "ProductAttributeValues",
+                column: "AttributeOptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductAttributeValues_ProductId_AttributeDefinitionId",
+                table: "ProductAttributeValues",
+                columns: new[] { "ProductId", "AttributeDefinitionId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductAttributeValues_ProductId1",
+                table: "ProductAttributeValues",
+                column: "ProductId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryAttributeDefinitions");
+
+            migrationBuilder.DropTable(
+                name: "ProductAttributeValues");
+
+            migrationBuilder.DropTable(
+                name: "AttributeOptions");
+
+            migrationBuilder.DropTable(
+                name: "AttributeDefinitions");
+        }
+    }
+}
